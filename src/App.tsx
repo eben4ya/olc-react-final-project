04c3925715b5e5d53febe03a05b779786f4dd080
eben4ya <benaya.imanuela@gmail.com>
2023-12-22 19:23:43 +0700

    <feat: add fetch method (still error)>

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,81 @@ import React, { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import NotesPage from "./components/NotesPage";
 
+// const getFolder = async () => {
+//   try {
+//     await fetch("https://cashier.rulim34.dev/directories", {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${process.env.Api_Key}`,
+//         "Content-Type": "application/json",
+//       },
+//     })
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error("Network response was not ok");
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log("Folder " + data);
+//       });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// const getFile = async () => {
+//   try {
+//     await fetch("https://cashier.rulim34.dev/notes", {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${process.env.Api_Key}`,
+//         "Content-Type": "application/json",
+//       },
+//     })
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error("Network response was not ok");
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log("File " + data);
+//       });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
 const App = () => {
   const [folder, setFolder] = useState<string[]>([]);
   const [file, setFile] = useState<string[][]>([[]]);
+
+  const fetchFolder = async () => {
+    try {
+      const response = await fetch("https://cashier.rulim34.dev/directories", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.ApiKey}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+      // Lakukan sesuatu dengan data yang diterima, misalnya:
+      // setFolder(data.folder);
+      // setFile(data.file);
+    } catch (error) {
+      console.error("There was a problem fetching the data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFolder(); // Panggil fungsi fetch saat komponen dimuat
+  }, []); // [] menandakan useEffect hanya dipanggil sekali saat komponen dimuat
 
   // Fungsi untuk menyimpan state ke localStorage
   // const saveToLocalStorage = (value: string[]) => {
